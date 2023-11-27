ea22402f81bc924cee0e4bbb3a032eed0ddbf8d3
nishio-yoshiki <nishio0915@icloud.com>
2023-11-27 16:22:00 +0900

    Update users_controller.rb
    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    user = User.find(params[:id])
   end
 
   def show
@@ -12,7 +13,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
 end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
